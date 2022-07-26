78cb50a4975a263e9424087feeca3e48a1a976e6
ash-old <ashley.oldham@gmail.com>
2022-07-26 16:21:03 +0100

    initial app with searchability function

--- BlueToothTest/ViewController.swift
+++ BlueToothTest/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    centralManager = CBCentralManager(delegate: self, queue: nil)
+    
+    view.backgroundColor = .systemPink
   }
+  
+  var centralManager: CBCentralManager!
+  var myPeripheral: CBPeripheral!
 
+}
 
+extension ViewController: CBCentralManagerDelegate, CBPeripheralDelegate {
+  func centralManagerDidUpdateState(_ central: CBCentralManager) {
+    // checks state of Bluetooth on device. On/Off?
+    if central.state == CBManagerState.poweredOn {
+      print("Bluetooth is ON", central.state)
+      
+      central.scanForPeripherals(withServices: nil, options: nil)
+    } else {
+      print("Bluetooth is OFF")
+    }
+  }
+  
+  func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+    if let pname = peripheral.name {
+      if pname == "SHIELD" {
+        central.stopScan()
+        
+        self.myPeripheral = peripheral
+        myPeripheral.delegate = self
+        
+        central.connect(peripheral, options: nil)
+      }
+      print(pname)
+    }
+  }
+  
+  func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+    self.myPeripheral.discoverServices(nil)
+  }
+  
+  
 }
 
